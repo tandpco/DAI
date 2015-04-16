3e220160109cc26e678c7c2bfab9b13d11a77adb
DudeLetsLive <dannycansignup@gmail.com>
2015-04-16 12:07:09 -0600

    Set up Gruntfile to compile styles and scripts

---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  var folders = {
+    lib: 'lib',
+    pub: 'public'
+  };
+
+  grunt.initConfig({
+    folders: folders,
+    watch: {
+      stylus: {
+        files: '<%= folders.lib %>/styles/**/*.styl',
+        tasks: ['stylus']
+      },
+      coffee: {
+        files: '<%= folders.lib %>/scripts/**/*.coffee',
+        tasks: ['coffee']
+      },
+      server: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '<%= folders.pub %>/styles/**/*.css',
+          '<%= folders.pub %>/js/**/*.js',
+          '<%= folders.pub %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
+          '*.php',
+        ]
+      },
+    },
+    stylus: {
+      compile: {
+        files: [{
+          expand: true,
+          cwd: '<%= folders.lib %>/styles',
+          src: ['{,*/}*.styl', '!**/_*'],
+          dest: '<%= folders.pub %>/styles',
+          ext: '.css'
+        }],
+        options: {
+          compress: false,
+          urlfunc: 'url'
+        }
+      }
+    },
+    coffee: {
+      compile: {
+        expand: true,
+        flatten: true,
+        src: '<%= folders.lib %>/scripts/**/*.coffee',
+        dest: '<%= folders.pub %>/js/',
+        ext: '.js'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['watch']);
+
+};
