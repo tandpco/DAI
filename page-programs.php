089079d6513cb64375ba9eef67c70af79bbc89a3
Randy Wiafe <wiafe23@gmail.com>
2015-04-28 17:56:42 -0600

    What we do

--- page-programs.php
+++ page-programs.php
@@ -32,7 +32,7 @@ get_header(); ?>
 	</div><!-- End Jumbotron -->
 
 	<section class="impact">
-		<div class="container">
+		<div class="container  hidden">
 			<p class="align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
 			<p class="align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
 			<div class="row clear-bottom">
@@ -62,9 +62,197 @@ get_header(); ?>
 				</div>
 			</div><!-- .row -->
 		</div>
-	</section><!-- section.impact -->
 
-	<section class="bar" style="margin-bottom: -30px; margin-top: 30px;">
+		<div class="container educate hidden">
+			<h2 class="shadow-beige tr-up">Educate</h2>
+			<hr>
+			<p>Whether in DAI's multi-day workshops or Masters of Art in Organizational Leadership Program, we strive to develop our leaders through education that combines princples and knowledge along with practical application. We model our leadership style after Jesus Christ, encouraging leaders to serve others from a place of spiritual connection and integrity.</p>
+			<div class="container">
+				<div class="row align-center">
+					<div class="col-md-6 workshop">
+						<img src="<?php echo get_template_directory_uri() ?>/public/images/workshop.png">
+						<h2 class="tr-up">Workshops</h2>
+						<h5 class="tr-up">Non-formal Eduaction</h5>
+					</div>
+					<div class="col-md-6">
+						<img src="<?php echo get_template_directory_uri() ?>/public/images/maol.png">
+						<h2 class="tr-up">MAOL</h2>
+						<h5 class="tr-up">Masters of Arts in Organizational Leadership</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container whatwedo hidden">
+			<h2 class="shadow-beige tr-up">Mentor</h2>
+			<hr>
+			<p>Even the most seasoned and gifted leader needs to be led and mentored.  Recognizing this truth, DAI provides mentoring relationships for students that blend intentional friendship, discipleship, and skills coaching. </p>
+			<div class="container">
+				<p class="testimonials">Testimonials</p>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“I have learned that the purpose of submission to God is freedom from the terrible burden of needing to get my own way, from the obsession that demands things must go as I believe they should. The result is a vast sense of relief. I am amazed at the wonderful freedom I now have as a result of learning this discipline. Thank you DAI.”</p>
+						<p>- Daniel, student in Spiritual Formation course, Uganda</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>"I am glad to let you know that my fellow Burundian citizens and I are benefiting through this MA programme. It is useful not only to us as students but also to the Burundian church and the whole nation. I hope that, given the relevance of the material and the determination I have of putting it into practice, and in communion with other Burundian brethren who are also following it, a new era is coming soon in the Burundi Christianity.”</p>
+						<p>- Emmie, MA Program student in Burundi</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“The Master’s program helped me realize the value of other people. I changed into a manager who doesn’t just want to see a task achieved, but also to realize that tasks are handled by people. Understanding and knowing these people is part of good leadership.”</p>
+						<p>- Eva Mulema, Chief of Party, USAID Governance</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container whatwedo hidden">
+			<h2 class="shadow-beige tr-up">Consult</h2>
+			<hr>
+			<p>DAI consulting staff specialize in counseling leaders in the areas of leadership, strategic planning, organizational structure, finance, adult learning, research, and evaluation. This consulting helps leaders and ministries thrive in areas of struggle.   </p>
+			<div class="container">
+				<p class="testimonials">Testimonials</p>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“I have learned that the purpose of submission to God is freedom from the terrible burden of needing to get my own way, from the obsession that demands things must go as I believe they should. The result is a vast sense of relief. I am amazed at the wonderful freedom I now have as a result of learning this discipline. Thank you DAI.”</p>
+						<p>- Daniel, student in Spiritual Formation course, Uganda</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>"I am glad to let you know that my fellow Burundian citizens and I are benefiting through this MA programme. It is useful not only to us as students but also to the Burundian church and the whole nation. I hope that, given the relevance of the material and the determination I have of putting it into practice, and in communion with other Burundian brethren who are also following it, a new era is coming soon in the Burundi Christianity.”</p>
+						<p>- Emmie, MA Program student in Burundi</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“The Master’s program helped me realize the value of other people. I changed into a manager who doesn’t just want to see a task achieved, but also to realize that tasks are handled by people. Understanding and knowing these people is part of good leadership.”</p>
+						<p>- Eva Mulema, Chief of Party, USAID Governance</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<div class="container whatwedo hidden">
+			<h2 class="shadow-beige tr-up">Connect</h2>
+			<hr>
+			<p>DAI is committed to connecting leaders and their organizations to other ministries and resources in an effort to find solutions to the challenges they face. No one solution fits all therefore, DAI also partners with other leadership development organizations in order to provide more opportunities to train leaders all over the world.</p>
+			<div class="container">
+				<p class="testimonials">Testimonials</p>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“I have learned that the purpose of submission to God is freedom from the terrible burden of needing to get my own way, from the obsession that demands things must go as I believe they should. The result is a vast sense of relief. I am amazed at the wonderful freedom I now have as a result of learning this discipline. Thank you DAI.”</p>
+						<p>- Daniel, student in Spiritual Formation course, Uganda</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>"I am glad to let you know that my fellow Burundian citizens and I are benefiting through this MA programme. It is useful not only to us as students but also to the Burundian church and the whole nation. I hope that, given the relevance of the material and the determination I have of putting it into practice, and in communion with other Burundian brethren who are also following it, a new era is coming soon in the Burundi Christianity.”</p>
+						<p>- Emmie, MA Program student in Burundi</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“The Master’s program helped me realize the value of other people. I changed into a manager who doesn’t just want to see a task achieved, but also to realize that tasks are handled by people. Understanding and knowing these people is part of good leadership.”</p>
+						<p>- Eva Mulema, Chief of Party, USAID Governance</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<div class="container whatwedo moal">
+			<h2 class="shadow-beige tr-up"><img src="<?php echo get_template_directory_uri() ?>/public/images/maol.png">MAOL</h2>
+			<hr class="line">
+			<div class="rail">
+				<ul class="tabs">
+					<li class="active">Overview</li>
+					<li>Program Goals</li>
+					<li>Why In-Country Partnerships</li>
+					<li>Course List</li>
+					<li>Where Moal is Offered</li>
+				</ul>
+			</div>
+			<hr class="line">
+			<div class="overview">
+				<p class="overview-text">Overview</p>
+				<p>The Master of Arts in Organizational Leadership (MAOL) is the most in-depth training we offer, where students experience character and skill development, as well as gain credibility from a master’s degree. The MAOL is offered in partnership with 20 local universities in 19 different countries serving approximately 1000 students each year.</p>
+				<a class="btn" href="#">Next<i class="fa fa-angle-right"></i></a>
+			</div>
+			<div class="container">
+				<p class="testimonials">Testimonials</p>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“I have learned that the purpose of submission to God is freedom from the terrible burden of needing to get my own way, from the obsession that demands things must go as I believe they should. The result is a vast sense of relief. I am amazed at the wonderful freedom I now have as a result of learning this discipline. Thank you DAI.”</p>
+						<p>- Daniel, student in Spiritual Formation course, Uganda</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>"I am glad to let you know that my fellow Burundian citizens and I are benefiting through this MA programme. It is useful not only to us as students but also to the Burundian church and the whole nation. I hope that, given the relevance of the material and the determination I have of putting it into practice, and in communion with other Burundian brethren who are also following it, a new era is coming soon in the Burundi Christianity.”</p>
+						<p>- Emmie, MA Program student in Burundi</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-2">
+						<img src="https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg">
+					</div>
+					<div class="col-md-10">
+						<p>“The Master’s program helped me realize the value of other people. I changed into a manager who doesn’t just want to see a task achieved, but also to realize that tasks are handled by people. Understanding and knowing these people is part of good leadership.”</p>
+						<p>- Eva Mulema, Chief of Party, USAID Governance</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<section class="bar reset" style="margin-bottom: -30px; margin-top: 30px;">
 		<div class="container align-center">
 			<h4 class="tr-up inline-block"><img src="<?php echo get_template_directory_uri() ?>/public/images/globe.png" /> Give where needed most</h4>
 			<span class="divider"></span>
